179c4a7638c4cf1b66aebd4a7effe73bd7d24cf7
NithyaKanamarlapudi <76540341+NithyaKanamarlapudi@users.noreply.github.com>
2022-07-21 12:02:27 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
 song1 = "";
 song2 = "";
+leftWristX = 0;
+rightWristX = 0;
+leftWristY = 0;
+rightWristY = 0;
  function preload() {
      song1 = loadSound("music.mp3");
      song2 = loadSound("music2.mp3");
  }
 
  function setup() {
-    canvas=createCanvas(400,400);
+    canvas = createCanvas(400, 400);
     canvas.center();
+
     video = createCapture(VIDEO);
     video.hide();
- }
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('Model is Loaded');
+}
+
+function gotPoses(results){
+    if (results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX= results[0].pose.rightWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristY= results[0].pose.rightWrist.y;
+    }
+    }
 
  function draw() {
      image(video, 0, 0, 400, 400);
